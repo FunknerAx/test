92e1366d171b6a0e00586264e33be84620422645
DESKTOP-8IKCPSQ\justf <just.funkner@gmail.com>
2022-06-02 21:45:34 +0600

    add swagger add time to log

--- app.js
+++ app.js
@@ -2,12 +2,16 @@ const express = require('express');
 const app = express();
 const config = require('./configs');
 const userRoute = require('./Routes/userRoute');
+const swaggerUI = require('swagger-ui-express');
+const YAML = require('yamljs');
+const swaggerDoc = YAML.load('./api-docs/swagger.yaml');
 
 const LoggerMiddleware = (req, res, next) => {
-    console.log(`${req.method} ${req.url}`)
+    console.log(`${new Date().toISOString()} ${req.method} ${req.url}`)
     next();
 }
 
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDoc));
 app.use(LoggerMiddleware);
 app.use(express.json());
 
